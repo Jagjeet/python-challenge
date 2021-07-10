d649060f31872ca00ab3348e3e74940d4edf2d24
Jagjeet Khalsa <Jagjeet.Khalsa@gmail.com>
2021-07-10 13:12:10 -0700

    Calculate total months

---
+++ PyBank/main.py
@@ -0,0 +1,33 @@
+import os
+import csv
+
+csvpath = os.path.join('.', 'Resources', 'budget_data.csv')
+total_months = 0
+
+with open(csvpath) as csvfile:
+
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    # Read the header row first (skip this step if there is now header)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    # Read each row of data after the header
+    for row in csvreader:
+        # print(row)
+        #Caluclate total number of months included in the dataset
+        total_months += 1
+
+
+# REVISIT - Add calculations for the following:
+# The net total amount of “Profit/Losses” over the entire period
+# Calculate the changes in “Profit/Losses” over the entire period, then find the average of those changes
+# The greatest increase in profits (date and amount) over the entire period
+# The greatest decrease in losses (date and amount) over the entire period
+
+    # REVISIT - Write out to analysis.txt instead of printing out
+    print("Financial Analysis")
+    print("----------------------------")
+    print(f"Total Months: {total_months}")
+
+    # REVISIT - Allow user to specify input/output files
