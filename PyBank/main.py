5992866399e5396cf6f2d1ad22628ccf2e90ef6e
Jagjeet Khalsa <Jagjeet.Khalsa@gmail.com>
2021-07-14 17:09:30 -0700

    Calculate total

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,6 +3,7 @@ import csv
 
 csvpath = os.path.join('.', 'Resources', 'budget_data.csv')
 total_months = 0
+total = 0
 
 with open(csvpath) as csvfile:
 
@@ -10,24 +11,27 @@ with open(csvpath) as csvfile:
 
     # Read the header row first (skip this step if there is now header)
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    # print(f"CSV Header: {csv_header}")
 
     # Read each row of data after the header
     for row in csvreader:
         # print(row)
-        #Caluclate total number of months included in the dataset
+
+        # Total number of months included in the dataset
         total_months += 1
 
+        # Net total amount of “Profit/Losses” over the entire period
+        total += float(row[1])
 
-# REVISIT - Add calculations for the following:
-# The net total amount of “Profit/Losses” over the entire period
-# Calculate the changes in “Profit/Losses” over the entire period, then find the average of those changes
-# The greatest increase in profits (date and amount) over the entire period
-# The greatest decrease in losses (date and amount) over the entire period
+        # REVISIT - Add calculations for the following:
+        # Calculate the changes in “Profit/Losses” over the entire period, then find the average of those changes
+        # The greatest increase in profits (date and amount) over the entire period
+        # The greatest decrease in losses (date and amount) over the entire period
 
     # REVISIT - Write out to analysis.txt instead of printing out
     print("Financial Analysis")
     print("----------------------------")
     print(f"Total Months: {total_months}")
+    print(f"Total: {total:.2f}")
 
     # REVISIT - Allow user to specify input/output files
