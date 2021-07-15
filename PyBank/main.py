5b9ee96e2b6d4a1e0bac9564d0bbc0a0e926ed16
Jagjeet Khalsa <Jagjeet.Khalsa@gmail.com>
2021-07-14 18:10:31 -0700

    Add greatest increase/decrease in profits

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,7 +6,7 @@ total_months = 0
 total = 0
 count = 0
 prev_profit_loss = 0
-profit_loss_changes = []
+profit_loss_changes = {}
 average_change = 0
 average_profit_loss = 0
 
@@ -32,7 +32,7 @@ with open(csvpath) as csvfile:
 
         # Calculate the changes in “Profit/Losses” over the entire period
         if (index != 0):
-          profit_loss_changes.append(profit_loss - prev_profit_loss)
+          profit_loss_changes[date] = profit_loss - prev_profit_loss
           prev_profit_loss = profit_loss
           count = index
         else:
@@ -40,17 +40,32 @@ with open(csvpath) as csvfile:
 
 
         # REVISIT - Add calculations for the following:
-        # The greatest increase in profits (date and amount) over the entire period
-        # The greatest decrease in losses (date and amount) over the entire period
 
     #Calculate average of the profit/loss changes
-    average_profit_loss = sum(profit_loss_changes)/count
+    average_profit_loss = sum(profit_loss_changes.values())/count
+
+    # Calulate greatest increase in profits (date and amount) over the entire period
+    # and greatest decrease in losses (date and amount) over the entire period
+    max_increase_date = None
+    max_decrease_date = None
+    for date in profit_loss_changes:
+      # Note this will only grab the first date if there is a tie
+      if (max_increase_date == None
+        or (profit_loss_changes[date] > profit_loss_changes[max_increase_date])):
+        max_increase_date = date
+
+      if (max_decrease_date == None
+        or (profit_loss_changes[date] < profit_loss_changes[max_decrease_date])):
+        max_decrease_date = date
+
 
     # REVISIT - Write out to analysis.txt instead of printing out
     print("Financial Analysis")
     print("----------------------------")
     print(f"Total Months: {total_months}")
     print(f"Total: {total:.2f}")
+    # print(f"Profit Loss Changes: {profit_loss_changes}")
     print(f"Average Change: {average_profit_loss:.2f}")
-
+    print(f"Greatest Increase in Profits: {max_increase_date} (${profit_loss_changes[max_increase_date]:.2f})")
+    print(f"Greatest Decrease in Profits: {max_decrease_date} (${profit_loss_changes[max_decrease_date]:.2f})")
     # REVISIT - Allow user to specify input/output files
